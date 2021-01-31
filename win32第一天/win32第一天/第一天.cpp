bd137fdb7cba1f4202c2c42df46bc77e3ab846ef
阿僧 <2665090094@qq.com>
2021-01-31 22:14:30 +0800

    123

---
+++ win32第一天/win32第一天/第一天.cpp
@@ -0,0 +1,6 @@
+#include<windows.h>
+int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevlnstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, L"Hello World! ", L"ÌבÊ¾", MB_OK);
+	return 0;
+}
